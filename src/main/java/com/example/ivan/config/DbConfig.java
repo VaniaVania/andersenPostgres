5bf938394e65f4f019a050cdbdd4b6824678eda3
Ivan <ivantumentsev@gmail.com>
2023-09-25 17:09:12 +0300

    Trust commit

--- src/main/java/com/example/ivan/config/DbConfig.java
+++ src/main/java/com/example/ivan/config/DbConfig.java
@@ -1,7 +1,7 @@
 package com.example.ivan.config;
 
 public class DbConfig {
-    public final static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public final static String DB_URL = "jdbc:postgresql://10.0.2.15:5432/postgres";
     public final static String DB_USERNAME = "postgres";
     public final static String DB_PASSWORD = "postgres";
     //
